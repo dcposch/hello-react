9f69cf5d2ef01851d86a3260c61813eec908cf10
DC <dcposch@dcpos.ch>
2022-01-10 17:53:42 -0800

    step 1: hello world

---
+++ app.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <h1>hello world</h1>;
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
